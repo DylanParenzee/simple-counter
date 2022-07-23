7e5a8835884f985bf38c6f84466e81278b17c569
DylanParenzee <dylanparenzee@hotmail.com>
2022-07-23 17:21:32 +1000

    added logic

--- simple-counter/src/Counter.js
+++ simple-counter/src/Counter.js
@@ -3,15 +3,34 @@ import { useState } from "react";
 
 const Counter = () => {
   //counter and counter state
-
   const [counter, setCounter] = useState(0);
+
+  // increase counter
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  };
+  // reset counter
+  const resetCounter = () => {
+    setCounter(0);
+  };
+
+  // decrease counter
+  const decreaseCounter = () => {
+    setCounter(counter - 1);
+  };
   return (
     <>
       <h1>Simple Counter</h1>
       <h1>{counter}</h1>
-      <button className="btn">Increase</button>
-      <button className="btn">Reset</button>
-      <button className="btn">Decrease</button>
+      <button className="btn" onClick={decreaseCounter}>
+        Decrease
+      </button>
+      <button className="btn" onClick={resetCounter}>
+        Reset
+      </button>
+      <button className="btn" onClick={increaseCounter}>
+        Increase
+      </button>
     </>
   );
 };
